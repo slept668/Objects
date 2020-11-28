6cc562d27016201494f8699a5821febed44b7a5c
Brian <oldhamb90@gmail.com>
2020-11-28 17:59:27 -0500

    Actually Mapped

--- Objects.js
+++ Objects.js
@@ -1,4 +1,5 @@
-var top5 = [
+const ol = document.querySelector("ol");
+const top5 = [
     {
         "name": "Billy Madison",
         "year": 1995,
@@ -31,56 +32,13 @@ var top5 = [
     },
 ];
 
-var array0 = [
-    top5[0].name, top5[0].year, top5[0].director, top5[0].pic
-];
-var array1 = [
-    top5[1].name, top5[1].year, top5[1].director, top5[1].pic
-];
-var array2 = [
-    top5[2].name, top5[2].year, top5[2].director, top5[2].pic
-];
-var array3 = [
-    top5[3].name, top5[3].year, top5[3].director, top5[3].pic
-];
-var array4 = [
-    top5[4].name, top5[4].year, top5[4].director, top5[4].pic
-];
+const template = top5.map(top => `
+<li>
+    <p>Name: ${top.name}</p>
+    <p>Released: ${top.year}</p>
+    <p>Director: ${top.director}</p>
+    <img src="${top.pic}">
+`);
 
-const olContent = `
-    <section>
-        <li>
-            <ol>${array0[0]}</ol>
-            <ol>Year: ${array0[1]}</ol>
-            <ol>Director: ${array0[2]}</ol>
-            <img src=${array0[3]}>
-        </li>
-            <li>
-            <ol>${array1[0]}</ol>
-            <ol>Year: ${array1[1]}</ol>
-            <ol>Director: ${array1[2]}</ol>
-            <img src=${array1[3]}>
-        </li>
-            <li>
-            <ol>${array2[0]}</ol>
-            <ol>Year: ${array2[1]}</ol>
-            <ol>Director: ${array2[2]}</ol>
-            <img src=${array2[3]}>
-        </li>
-            <li>
-            <ol>${array3[0]}</ol>
-            <ol>Year: ${array3[1]}</ol>
-            <ol>Director: ${array3[2]}</ol>
-            <img src=${array3[3]}>
-        </li>
-        <li>
-            <ol>${array4[0]}</ol>
-            <ol>Year: ${array4[1]}</ol>
-            <ol>Director: ${array4[2]}</ol>
-            <img src=${array4[3]}>
-        </li>
-    </section>
-`;
-
-const ol = document.querySelector('ol');
-ol.innerHTML = olContent;
+console.log(template);
+ol.innerHTML = template.join('');
